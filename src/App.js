79a67aeca40842e80e1660023432918b13bddd86
Sam Singer <samuel.singer1@gmail.com>
2018-09-14 18:12:18 -0700

    Complete first project updating and changing state

--- src/App.js
+++ src/App.js
@@ -8,26 +8,75 @@ class App extends Component {
       { name: "Sham", age: 28 },
       { name: "Mondo", age: 27 },
       { name: "Steph", age: 26 }
-    ]
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked')
+    // DONT DO THIS: this.state.persons[0].name = 'Maximillian'
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Gonzo", age: 27 },
+        { name: "Dan", age: 26 }
+      ]
+    })
+  }
+  // Not actively calling use HANDLER
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Shamu", age: 28 },
+        { name: event.target.value, age: 27 },
+        { name: "Dan", age: 26 }
+      ]
+    })
   }
 
   render() {
-    // single root element 
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '2px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>I'm a React App!!!</h1>
         <p>This is working!</p>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Climbing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Shamulian')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Samil')}
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}
+        >My Hobbies: Climbing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+        />
       </div>
     );
-    // Passing props and content utilizing props and props.children = the content between the tags instead of passed through as properties.
-    // Names are hard coded. Let's change that. 
-
-    // Core Concept: props and state are what triggers React to re-render components
   }
 }
+
+// Ability to pass methods through as props along with state.
+
+// Utilize bind() or an anonymous arrow function 
+// Maybe more efficient to use bind() rather than the arrow function
+
+// Creating a manual typed respnse for passing this and updating state
+
+// Inline styling is possible but you may use an external stylesheet as well
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,26 +1,19 @@
-// A component is just a function that returns some JSX
-
-// function person() {
-// }
-
-// var person = function() {
-// }
-
-// Above are perfecty fine to use but best practice currently
-
 import React from 'react';
+import './Person.css';
 
 const person = ( props ) => {
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>    
     )
 }
 
+// Utilize these components as frequently as possible to keep code 
+// readable and easy to test.
+
+// Most state is not updated through components.
+
 export default person;
-
-// Static texts is fine but creating a dynamic component is the goal
-// Make sure you output dynamic content with {}
-
