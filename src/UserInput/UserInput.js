1413ea4f1281881aa4394527ce2690a20ad7820a
Sam Singer <samuel.singer1@gmail.com>
2018-09-17 17:00:47 -0700

    Complete and correct

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const userInput = (props) => {
+    return (
+        <div> 
+            <input
+                type="text"
+                value={props.name}
+                onChange={ props.changeNames }
+            />
+        </div>
+    )
+}
+
+export default userInput;
