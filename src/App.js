4f3a26739c3cfcbace99f0159d897671be942e6b
Sam Singer <samuel.singer1@gmail.com>
2018-09-19 11:29:08 -0700

    Update state in an immutable fashion

--- src/App.js
+++ src/App.js
@@ -33,7 +33,11 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const names = this.state.name;
+    // This is a pointer to the original STATE
+    // Poor practice, so create a copy with the slice() method
+    const names = this.state.name.slice();
+    // OR
+    // const names = [...this.state.name];
     names.splice(personIndex, 1);
 
     this.setState({
@@ -81,76 +85,3 @@ class App extends Component {
 }
 
 export default App;
-
-// class App extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       person: [
-//         { name: 'Fam', age: 26 },
-//         { name: 'Gam', age: 25 },
-//         { name: 'Ham', age: 24 },
-//       ]
-//     }
-//   }
-
-//   switchNameHandler = (newNamesArray, agesArray = null) => {
-//     if (agesArray) {
-//       this.setState({
-//         person: [
-//           { name: newNamesArray[0], age: agesArray[0] },
-//           { name: newNamesArray[1], age: agesArray[1] },
-//           { name: newNamesArray[2], age: agesArray[2] },
-//         ]
-//       })
-//     }
-//     else {
-//       this.setState({
-//         person: [
-//           { name: newNamesArray[0], age: 1 },
-//           { name: newNamesArray[1], age: 2 },
-//           { name: newNamesArray[2], age: 3 },
-//         ]
-//       })
-//     }
-//   }
-
-//   changeNameHandler = (event) => {
-//     this.setState({
-//       person: [
-//         { name: event.target.value, age: 1 },
-//         { name: event.target.value, age: 2 },
-//         { name: event.target.value, age: 3 },
-//       ]
-//     })
-//   }
-
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Hello I'm A React App</h1>
-//         <UserInput />
-//         <UserOutput />
-//         <button
-//           onClick={this.switchNameHandler.bind(this, ["Bub", "Lub", "Cub"])}
-//         >Switch All Names</button>
-//         <Person
-//           name={this.state.person[0].name}
-//           age={this.state.person[0].age}
-//         />
-//         <Person
-//           name={this.state.person[1].name}
-//           age={this.state.person[1].age}
-//           changeName={this.changeNameHandler}
-//         > Newtonian Physics  </Person>
-//         <Person
-//           name={this.state.person[2].name}
-//           age={this.state.person[2].age}
-//           // switchName={ this.switchNameHandler.bind(this, ['Cring', 'Ming', 'Ting'], [6,7,8]) }
-//           switchName={() => this.switchNameHandler(['Cring', 'Ming', 'Ting'], [6, 7, 8])}
-//         />
-//       </div>
-//     )
-//   }
-// }
