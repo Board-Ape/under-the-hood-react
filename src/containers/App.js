676defba43e7f2af62ba6a56036f21114fab345c
Sam Singer <samuel.singer1@gmail.com>
2018-09-29 07:02:53 -0700

    Delete button is not working but the Aux wrapper and the WithClass is

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Cars from '../components/Cars/Cars';
 import Cockpit from '../components/Cockpit/Cockpit';
+import Aux from '../hoc/Aux';
 import WithClass from '../hoc/WithClass';
 
 class App extends Component {
@@ -95,18 +96,18 @@ class App extends Component {
         }
 
         return (
-            <div className='App'>
+            <Aux>
                 <Cockpit 
                     cars={this.state.cars}
                     show={this.toggleShowCarHandler}
                 />
                 { cars }
-            </div>
+            </Aux>
         )
     }
 }
 
-export default (App);
+export default WithClass(App, 'App');
 
 
 
