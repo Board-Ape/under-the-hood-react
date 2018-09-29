214b23357b269e8683d33ee5f7a8265797cb75d8
Sam Singer <samuel.singer1@gmail.com>
2018-09-29 06:53:17 -0700

    Aux is essential invisible

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const withClass = (props) => {
-    <div className={props.classes}>
-        { props.children }
-    </div>
-};
+const withClass = ( WrappedComponent, className ) => {
+    return ( props ) => (
+        <div className={ className }>
+            <WrappedComponent />
+        </div>
+    )
+}
 
 export default withClass;
+
+
+// import React from 'react';
+
+// const withClass = (props) => {
+//     <div className={props.classes}>
+//         { props.children }
+//     </div>
+// };
+
+// export default withClass;
